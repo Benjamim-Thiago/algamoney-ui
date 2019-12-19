248342b9a88e82fb3238581a0d12885daeb8f915
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-12-19 17:24:15 -0300

    fazendo validação do formulario de cadastro de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+
+import { CurrencyMaskModule } from "ng2-currency-mask";
 
 import {InputTextModule} from 'primeng/inputtext';
 import {InputTextareaModule} from 'primeng/inputtextarea';
+import {InputMaskModule} from 'primeng/inputmask';
 import {SelectButtonModule} from 'primeng/selectbutton';
+import {DropdownModule} from 'primeng/dropdown';
 import {CalendarModule} from 'primeng/calendar';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
@@ -15,24 +20,30 @@ import { ListPostingsComponent } from './list-postings/list-postings.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ListPeopleComponent } from './list-people/list-people.component';
 import { RegisterPostingComponent } from './register-posting/register-posting.component';
+import { RegisterPersonComponent } from './register-person/register-person.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ListPostingsComponent,
     ListPeopleComponent,
-    RegisterPostingComponent
+    RegisterPostingComponent,
+    RegisterPersonComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     InputTextModule,
     InputTextareaModule,
+    InputMaskModule,
     SelectButtonModule,
+    DropdownModule,
     CalendarModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    CurrencyMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register-posting/register-posting.component.ts
+++ src/app/register-posting/register-posting.component.ts
@@ -11,6 +11,27 @@ export class RegisterPostingComponent implements OnInit {
     {label: 'Receita', value: 'RECEITA'},
     {label: 'Despesa', value: 'DESPESA'}
   ];
+
+  categories = [
+    {label: 'Alimentação', value: '1'},
+    {label: 'Transporte', value: '2'}
+  ];
+
+  people = [
+    {label: 'Benjamim Thiago', value: '1'},
+    {label: 'Raquel', value: '2'},
+    {label: 'Francisca', value: '3'},
+    {label: 'José Arlindo', value: '4'},
+    {label: 'Jacentes Tevez', value: '5'},
+    {label: 'Mario kilan', value: '6'},
+    {label: 'Amanda Nunes', value: '7'},
+    {label: 'Jon Jones', value: '8'},
+    {label: 'Lyoto Machida', value: '9'},
+    {label: 'Marcus', value: '10'},
+    {label: 'Maire', value: '11'},
+    {label: 'Mariano', value: '12'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
